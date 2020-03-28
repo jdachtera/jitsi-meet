a4907fa65a31278ccdbd42f459a31fc63ee0feb1
rawboard <rawr@sharkklaser.com>
2020-03-28 14:07:57 +0100

    fixed validationstrigs, entered new images and rooms

--- react/features/welcome/components/combs.js
+++ react/features/welcome/components/combs.js
@@ -1,38 +1,55 @@
 export const combs = [
     {
         title: 'Mad Honey',
-        img: 'https://pbs.twimg.com/media/CX_6lyMWwAAiGQu.jpg',
+        img: '.../images/mad_honey.jpg',
         path: 'MadHoney'
     },
     {
         title: 'Cabaret',
-        path: 'Cabaret'
+        path: 'Cabaret',
+        img: '../images/cabaret.jpg'
     },
     {
         title: 'the Troll Inn',
-        path: 'TrollInn'
+        path: 'TrollInn',
+        img: '../images/troll_inn.jpg'
     },
 
     {
         title: 'the Campfire',
-        path: 'Campfire'
+        path: 'Campfire',
+        img: '../images/camp_fire.jpg'
     },
     {
         title: 'Immediacy Room',
-        path: 'ImmediacyRoom'
+        path: 'ImmediacyRoom',
+        img: '../images/immidiacy.png'
     },
     { title: 'The Pollinator',
       url: 'https://twitch.tv',
-       path: '' 
+       path: '../images/Pollinator.jpg' 
     },
     {
-        title: 'Strange Bees from Outer Space',
-        url: 'https://www.zoom.us',
-        path:''
+        title: 'Vagabonds',
+        url: ' https://zoom.us/j/108160626',
+        path:'',
+        img: '../images/vagabonds.png'
     },
     {
         title: 'Hive Playa',
-        url: 'https://www.zoom.us',
-        path:''
+        url: 'https://zoom.us/j/988190716',
+        path:'',
+        img: '../images/Playa.jpg'
+    },
+    {
+        title: 'Sacred Space',
+        img: '../images/sacred_space.jpg',
+        path:'SacredSpace'
+    },
+    {
+        title: 'Kosmo Zoo',
+        img: '../images/kosmozoo.jpg',
+        path:'',
+        url: 'https://www.twitch.tv'
     }
 ];
